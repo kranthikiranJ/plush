5e431220ba6d75964a4f997a5ef7105e07b89ee4
kranthikiranJ <kranthikiran1993@gmail.com>
2024-03-27 23:42:34 +0530

    logo

--- script.js
+++ script.js
@@ -1,148 +1,241 @@
-// JavaScript
-    const searchForm = document.getElementById("search-form");
-    const searchInput = document.getElementById("search");
-    const resultsContainer = document.getElementById("results");
-    const similarResultsContainer = document.getElementById("results2");
-    const favouritesResultsContainer = document.getElementById("results3");
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search");
+const firstResultsContainer = document.getElementById("search-results-container");
+
+function displayFirstResult(firstPhoto){
+    const resultsContainer = document.createElement('div')
+    resultsContainer.innerHTML='';
+    const img = document.createElement('img');
+    img.classList.add('result-img')
+    img.src = firstPhoto.src.medium;
+    img.alt = firstPhoto.alt_description;
+    const imgInfo = document.createElement('div')
+    const text = document.createElement('div')
+    imgInfo.classList.add('img-info')
+    const altText = document.createElement('p');
+    altText.textContent = `${firstPhoto.alt}`;
+
+    const photographerName = document.createElement('p');
+    photographerName.classList.add('photographer')
+    photographerName.textContent = `${firstPhoto.photographer}`;
+
+    const exploreButton = document.createElement('button');
+    exploreButton.textContent = 'Explore More';
+    exploreButton.classList.add('explore-btn')
+    exploreButton.addEventListener('click', function() {
+    window.open(`https://www.pexels.com/@${firstPhoto.photographer_url}`, '_blank');
+    });
+
+    text.appendChild(altText);
+    text.appendChild(photographerName);
+    text.appendChild(exploreButton);
+    imgInfo.appendChild(img)
+    imgInfo.appendChild(text)
+    resultsContainer.appendChild(imgInfo)
+    firstResultsContainer.appendChild(resultsContainer)
+
+}
+
+function displaySimilarResluts(photos){
+    const carouselContainer = document.getElementById('carousel-container')
+    carouselContainer.innerHTML = '';
+    carouselContainer.classList.add('carousel-container');
+    photos.forEach(photo => {
+    const card = document.createElement('div');
+    card.classList.add('carousel-item');
+    const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+    const img = document.createElement('img');
+    img.src = photo.src.medium;
+    img.alt = photo.alt_description;
+    img.classList.add('card-image')
+    imageContainer.appendChild(img)
+
+    const altText = document.createElement('p');
+    altText.textContent = `${photo.alt}`;
+    altText.classList.add('alt-text')
+    const photographerName = document.createElement('p');
+    photographerName.textContent = `${photo.photographer}`;
+
+    const addButton = document.createElement('button');
+    const heartImg = document.createElement('img')
+    heartImg.src = "https://res.cloudinary.com/dqnqix89g/image/upload/v1711552625/Favorite_hynql7.png";
+    heartImg.classList.add('add')
+    addButton.classList.add('heart-btn')
+    addButton.appendChild(heartImg)
+   
+        addButton.addEventListener('click', function() {
+            displayFavouriteList(photo);
+            // recommendedSection.removeChild(imageContainer);
+            carouselContainer.removeChild(card)
+        });
+        imageContainer.appendChild(addButton)
+
+    card.appendChild(imageContainer);
+    card.appendChild(altText);
+    card.appendChild(photographerName);
+      carouselContainer.appendChild(card);
+    });
+
 
     
-    searchForm.addEventListener("submit", function(event) {
-      event.preventDefault(); // Prevent default form submission behavior
-      
-      const searchQuery = searchInput.value.trim();
-      if (searchQuery !== "") {
-        fetchData(searchQuery)
-          .then(data => {
-            console.log(data.photos); // Log the fetched data to the console
-            // You can further process the data or display it as needed
-            displayResults(data.photos);
-            function displayResults(photos) {
-                resultsContainer.innerHTML = ''; // Clear previous results
-                
-                if (photos.length > 0) {
-                    const firstPhoto = photos[0];
-                    const img = document.createElement('img');
-                    // img.classList.add('result-img')
-                    img.src = firstPhoto.src.medium;
-                    img.alt = firstPhoto.alt_description;
-                    resultsContainer.appendChild(img);
 
-                    // Create elements for alt text, photographer name, and photographer URL
-                    const imgInfo = document.createElement('div')
-                    const text = document.createElement('div')
-                    imgInfo.classList.add('img-info')
-                    const altText = document.createElement('p');
-                    altText.textContent = `${firstPhoto.alt}`;
-
-                    const photographerName = document.createElement('p');
-                    photographerName.textContent = `${firstPhoto.photographer}`;
-
-
-
-                    // Create "Explore More" button
-                    const exploreButton = document.createElement('button');
-                    exploreButton.textContent = 'Explore More';
-                    exploreButton.classList.add('explore-btn')
-                    exploreButton.addEventListener('click', function() {
-                        window.open(`https://www.pexels.com/@${firstPhoto.photographer_url}`, '_blank');
-                    });
-
-                    // Append elements to results container
-                    text.appendChild(altText);
-                    text.appendChild(photographerName);
-                    text.appendChild(exploreButton);
-                    imgInfo.appendChild(img)
-                    imgInfo.appendChild(text)
-                    resultsContainer.appendChild(imgInfo)
-
-                  } 
-                  if (photos.length > 1) {
-                    displayCarousel(photos.slice(1));
-                    // displayCarousel(photos.slice(1));
-                  } 
-        
-                  else {
-                    resultsContainer.innerHTML = '<p>No results found</p>';
-                  }
-                  function displayCarousel(photos) {
-                    const similarsection = document.createElement('h3');
-                    similarsection.textContent = 'Similar results';
-                    similarResultsContainer.appendChild(similarsection);
-                    const carouselContainer = document.createElement('div');
-
-                    carouselContainer.classList.add('carousel-container');
-                    
-                    photos.forEach(photo => {
-                        const itemContainer = document.createElement('div');
-                        itemContainer.classList.add('carousel-item');
-                      const img = document.createElement('img');
-                      img.src = photo.src.medium;
-                      img.alt = photo.alt_description;
-
-                    const altText = document.createElement('p');
-                    altText.textContent = `${photo.alt}`;
-                    altText.classList.add('alt-text')
-
-                    const photographerName = document.createElement('p');
-                    photographerName.textContent = `${photo.photographer}`;
-
-
-                    itemContainer.appendChild(img);
-                    itemContainer.appendChild(altText);
-                    itemContainer.appendChild(photographerName);
-                      carouselContainer.appendChild(itemContainer);
-                    });
-
-
-                    
-
-                    similarResultsContainer.appendChild(carouselContainer);
-                    favouritesResultsContainer.appendChild(carouselContainer)
-
-            
-                  }
-                  
-              }
-          })
-          .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-          });
-      }
-    });
-  
-    async function fetchData(searchQuery) {
-      const response = await fetch(`https://api.pexels.com/v1/search?query=${searchQuery}&per_page=10`, {
-        headers: {
-          Authorization:  'kyPF1NsYrDyjA9ndRgNhJpj1TA6nc7Qxqquhk4uM7BMzgH0OXzNzVvzg' 
-        }
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data; // Return the fetched data
-    }
-
-   
-
-
-
-
-
-    // Add event listener to the results container
-similarResultsContainer.addEventListener('click', function(event) {
-    const clickedImage = event.target;
-    if (clickedImage.tagName === 'IMG') {
-        // Store the clicked image source in local storage
-        localStorage.setItem('clickedImageSrc', clickedImage.src);
-    }
-});
-
-// Retrieve the clicked image source from local storage
-const storedImageSrc = localStorage.getItem('clickedImageSrc');
-if (storedImageSrc) {
-    // Use the stored image source as needed
-    console.log('Stored image source:', storedImageSrc);
 }
 
 
+function displayFavouriteList(photo){
+    // console.log(photo)
+    const favouriteContainer = document.getElementById('favorite-container')
+    favouriteContainer.classList.add('carousel-container');
+
+    const card = document.createElement('div');
+    card.classList.add('carousel-item');
+
+    const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+    const img = document.createElement('img');
+    img.src = photo.src.medium;
+    img.alt = photo.alt_description;
+    img.classList.add('card-image')
+    imageContainer.appendChild(img)
+
+    const altText = document.createElement('p');
+    altText.textContent = `${photo.alt}`;
+    altText.classList.add('alt-text')
+    const photographerName = document.createElement('p');
+    photographerName.textContent = `${photo.photographer}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = "X"
+    
+    removeButton.classList.add('heart-btn')
+
+   
+        removeButton.addEventListener('click', function() {
+            removeFavourite(card,photo)
+            favouriteContainer.removeChild(card)
+            
+        });
+        imageContainer.appendChild(removeButton)
+        
+        card.appendChild(imageContainer);
+    card.appendChild(altText);
+    card.appendChild(photographerName);
+    favouriteContainer.appendChild(card)
+
+    let favoriteItems = JSON.parse(localStorage.getItem('favorite-container')) || [];
+    favoriteItems.push(photo)
+    localStorage.setItem('favorite-container',JSON.stringify(favoriteItems))
+}
+
+function removeFavourite(card,photo){
+    const favouriteContainer = document.getElementById('favorite-container');
+    favouriteContainer.removeChild(card)
+
+    let favoriteItems = JSON.parse(localStorage.getItem('favorite-container')) || [];
+    favoriteItems = favoriteItems.filter(item=>item.id != photo.id);
+    localStorage.setItem('favourite-container',JSON.stringify(favoriteItems))
+}
+function favoriteListFromLocalStorage(){
+    const favouriteContainer = document.getElementById('favorite-container');
+    favouriteContainer.classList.add('carousel-container');
+
+    const favoriteItems = JSON.parse(localStorage.getItem('favorite-container')) || [];
+
+    favoriteItems.forEach(photo=>{
+        const card = document.createElement('div');
+    card.classList.add('carousel-item');
+
+    const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+    const img = document.createElement('img');
+    img.src = photo.src.medium;
+    img.alt = photo.alt_description;
+    img.classList.add('card-image')
+    imageContainer.appendChild(img)
+
+    const altText = document.createElement('p');
+    altText.textContent = `${photo.alt}`;
+    altText.classList.add('alt-text')
+    const photographerName = document.createElement('p');
+    photographerName.textContent = `${photo.photographer}`;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = "X"
+    
+    removeButton.classList.add('heart-btn')
+
+   
+        removeButton.addEventListener('click', function() {
+            removeFavourite(card,photo)
+            favouriteContainer.removeChild(card)
+            
+        });
+        imageContainer.appendChild(removeButton)
+
+    card.appendChild(imageContainer);
+    card.appendChild(altText);
+    card.appendChild(photographerName);
+    favouriteContainer.appendChild(card)
+
+    })
+
+
+}
+    searchForm.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        const searchQuery = searchInput.value.trim();
+        if (searchQuery !== "") {
+            fetchData(searchQuery)
+            .then(data => {
+                console.log(data)
+                displayFirstResult(data.photos[0])
+                displaySimilarResluts(data.photos.slice(1))
+            })
+    }
+})
+
+async function fetchData(searchQuery) {
+    try{
+        const response = await fetch(`https://api.pexels.com/v1/search?query=${searchQuery}&per_page=10`, {
+        headers: {
+            Authorization:  'kyPF1NsYrDyjA9ndRgNhJpj1TA6nc7Qxqquhk4uM7BMzgH0OXzNzVvzg' 
+        }
+        });
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;    
+    }
+    catch(error){
+        console.log(error)
+        
+    }
+}
+
+// window.onload = favoriteListFromLocalStorage;
+window.onload = function() {
+    // Clear local storage data
+    localStorage.clear();
+
+    // Load favorite list from local storage
+    favoriteListFromLocalStorage();
+};
+
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.mobile-navbar').classList.toggle('menu-open');
+  });
+
+  let hamber = document.querySelector('.hamburger')
+  hamber.addEventListener('click',function(){
+    hamber.classList.toggle('open')
+  })
+
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+
+    hamburgerMenu.addEventListener('click', () => {
+        nav.classList.toggle('show');
+        hamburgerMenu.classList.toggle('open');
+    });
